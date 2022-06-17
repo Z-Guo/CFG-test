c16245b0e8fecf7d55a5094be17db61890e1fd87
Ziling Guo <ziling.guo@redington.co.uk>
2022-06-17 23:45:38 +0100

    Homework week 3

---
+++ Ziling - Homework Week 3.py
@@ -0,0 +1,14 @@
+
+"""
+Question 1
+"""
+is_raining = input('Is it raining? y/n')
+
+if is_raining == 'y':
+    print('take an umbrella')
+elif is_raining == 'n':
+    print('You dont need an umbrella')
+
+
+
+
